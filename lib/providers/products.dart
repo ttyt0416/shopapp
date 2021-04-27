30335d34075e37f50d272b0d584be247c4108201
ttyt0416 <ttyt0416@gmail.com>
2021-04-27 17:07:07 +0900

    adding filtering function

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
 
-import '../models/product.dart';
+import './product.dart';
 
 class Products with ChangeNotifier {
   List<Product> _items = [];
 
+  // var _showFavoritesOnly = false;
+
   List<Product> get items {
+    // if (_showFavoritesOnly) {
+    //   return _items.where((prodItem) => prodItem.isFavorite).toList();
+    // }
     return [..._items];
+  }
+
+  List<Product> get favoriteItems {
+    return _items.where((prodItem) => prodItem.isFavorite).toList();
   }
 
   Product findById(String id) {
     return _items.firstWhere((prod) => prod.id == id);
   }
+
+  // void showFavoritesOnly() {
+  //   _showFavoritesOnly = true;
+  // }
+
+  // void showAll() {
+  //   _showFavoritesOnly = false;
+  // }
 
   void addProduct() {
     // _items.add(value);
