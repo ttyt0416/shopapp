ca3a2e526224e671b7c1c607bad6ac545aba48f4
unknown <ttyt0416@gmail.com>
2021-05-17 22:20:12 +0900

    adding http requests for order and products screens

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -32,6 +32,9 @@ class Orders with ChangeNotifier {
     final response = await http.get(url);
     final List<OrderItem> loadedOrders = [];
     final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    if (extractedData == null) {
+      return;
+    }
     extractedData.forEach((orderId, orderData) {
       loadedOrders.add(OrderItem(
         id: orderId,
--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -6,32 +6,44 @@ import '../providers/orders.dart' show Orders;
 import '../widgets/order-item.dart';
 import '../widgets/app_drawer.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
 
-  @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  @override
-  void didChangeDependencies() {
-    Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-    super.didChangeDependencies();
-  }
+  // @override
+  // void didChangeDependencies() {
+  //   setState(() {
+  //         _isLoading = true;
+  //       });
+  //   Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+  //   setState(() {
+  //     _isLoading = false;
+  //   });
+  //   super.didChangeDependencies();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    // final orderData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(
+      body: FutureBuilder(future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(), builder: (ctx, dataSnapshot) {
+        if (dataSnapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else {
+          if (dataSnapshot.error != null) {
+            //error handling
+            return Center(child: Text('Error occured',),);
+          } else {
+            return Consumer<Orders>(builder: (ctx, orderData, child) => ListView.builder(
         itemCount: orderData.orders.length,
         itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
-      ),
+      ),);
+          }
+        }
+      },), 
     );
   }
 }
