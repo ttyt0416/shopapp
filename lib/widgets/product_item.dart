5cc938139ee8d7b779c687857984b5bb4b48d79b
unknown <ttyt0416@gmail.com>
2021-06-08 21:54:05 +0900

    adding animations to product overview(Hero Animation and image appbar) and order screen

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -27,17 +27,26 @@ class ProductItem extends StatelessWidget {
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
-          onTap: () {
-            Navigator.of(context).pushNamed(
-              ProductDetailScreen.routeName,
-              arguments: product.id,
-            );
-          },
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover,
-          ),
-        ),
+            onTap: () {
+              Navigator.of(context).pushNamed(
+                ProductDetailScreen.routeName,
+                arguments: product.id,
+              );
+            },
+            child: Hero(
+              tag: product.id,
+              child: FadeInImage(
+                placeholder:
+                    AssetImage('assets/images/product-placeholder.png'),
+                image: NetworkImage(product.imageUrl),
+                fit: BoxFit.cover,
+              ),
+            )
+            // Image.network(
+            //   product.imageUrl,
+            //   fit: BoxFit.cover,
+            // ),
+            ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
           leading: IconButton(
